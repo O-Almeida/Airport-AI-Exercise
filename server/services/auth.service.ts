53bd2c33e486ca84cb929d91a312ce3b3f23f02d
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-15 20:18:15 +0000

    signIn method created on AuthService

--- server/services/auth.service.ts
+++ server/services/auth.service.ts
@@ -2,9 +2,39 @@ import { CreateUserDto } from "../dto/user/create-user.dto";
 import { IUser } from "../models/user/user.interface";
 import UserRepository from "../models/user/user.model";
 import * as bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { AuthCredentialsDto } from "../dto/user/auth-credential.dto";
+import { AUTH_EXPIRE, AUTH_SECRET } from "../constants/auth.constants";
 
 export class AuthService {
+  /**
+   * Authenticates a user with the provided credentials and returns an access token if successful.
+   * @param authCredentialsDto - The credentials to use for authentication.
+   * @returns A Promise that resolves to an access token if authentication is successful.
+   * @throws An error with message "Please check your login credentials." if authentication fails.
+   */
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findByUsername(username);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const accessToken = jwt.sign(
+        {
+          id: user.username,
+        },
+        AUTH_SECRET,
+        {
+          expiresIn: AUTH_EXPIRE,
+        }
+      );
+
+      return accessToken;
+    } else {
+      throw new Error("Please check your login credentials.");
+    }
+  }
+
   /**
    * Creates a new user entity in the database with the provided data.
    * @param createUserDto - The data necessary to create a new user entity.
