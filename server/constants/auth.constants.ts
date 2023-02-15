96c2c763bd57d7fec6a7fce82a16e69a9bc744e5
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-15 20:14:10 +0000

    auth.constants file created

---
+++ server/constants/auth.constants.ts
@@ -0,0 +1,8 @@
+/**
+ * The secret jey used to sign and verify JWTs for authentication.
+ */
+export const AUTH_SECRET = "YH55CL9x9q6";
+/**
+ * The duration (in seconds) that JWTs used for authentication time.
+ */
+export const AUTH_EXPIRE = 60 * 60 * 24 * 7;
