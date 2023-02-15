dc1a95d2cf645aebcb6578c5e5697da6e54f3a34
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-15 19:20:46 +0000

    user-roles.enum created

---
+++ server/models/user/user-roles.enum.ts
@@ -0,0 +1,8 @@
+// UserRoles enum has all the possible types of roles for a User
+/**
+ * A enum with the different roles that a user can have.
+ * */
+export enum UserRoles {
+  AGENT = "agent",
+  PASSENGER = "passenger",
+}
