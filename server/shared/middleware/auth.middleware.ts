86bc94dd88289ad3555dce9142744a0b63ea1623
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-16 18:11:15 +0000

    auth.middlware created

--- server/shared/middleware/auth.middleware.ts
+++ server/shared/middleware/auth.middleware.ts
@@ -1,19 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../../models/error/custom-error.model";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { AUTH_SECRET } from "../../constants/auth.constants";
 
-export function verifyToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization;
+export function authMiddlware(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    throw new CustomError("Authentication necessary to do this operation", 403);
-  }
-
-  jwt.verify(token, AUTH_SECRET, (err) => {
-    if (err) {
-      throw new CustomError("Unauthorized access", 401);
+    if (!token) {
+      throw new CustomError("Authentication token missing");
     }
+
+    const decoded = jwt.verify(token, AUTH_SECRET);
+
+    res.locals.jwtPayload = decoded;
+
     next();
-  });
+  } catch (error) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
 }
