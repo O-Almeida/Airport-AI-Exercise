872e4b23d0ee750994661f46ecd25aa40610b119
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-16 18:10:58 +0000

    global routes updated

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,9 +3,12 @@
  */
 import { Router } from "express";
 import { AuthRoutes } from "./auth.routes";
+import { ProductRoutes } from "./product.routes";
 
 const router = Router();
 
 router.use("/auth", new AuthRoutes().router);
+
+router.use("/product", new ProductRoutes().router);
 
 export default router;
