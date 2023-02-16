dda10d80ba72abf70c9911bbede7dde32af4e7ed
O-Almeida <brunof.santos.almeida@gmail.com>
2023-02-16 10:30:19 +0000

    removed class-validator decorators

--- server/dto/user/create-user.dto.ts
+++ server/dto/user/create-user.dto.ts
@@ -1,13 +1,3 @@
-import {
-  IsEnum,
-  IsOptional,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from "class-validator";
-import { UserRoles } from "../../models/user/user-roles.enum";
-
 /**
  * A Data Transfer Object representing the data needed to creat a new user.
  *
@@ -18,24 +8,7 @@ import { UserRoles } from "../../models/user/user-roles.enum";
  *
  */
 export class CreateUserDto {
-  @IsString()
-  @MinLength(3)
-  @MaxLength(20)
   username: string;
-
-  @IsString()
-  @MinLength(5)
-  @MaxLength(30)
-  /* Password need to contains at leats one digit or non-word character,
-  at least one uppercase letter, at least one lowercase letter,
-  and not contain any periods or newlines.
-  */
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: "Password is too weak.",
-  })
   password: string;
-
-  @IsEnum(UserRoles, { message: "Invalid role" })
-  @IsOptional()
   role?: string;
 }
